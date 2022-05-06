0c9895d6407e30e979d190da9c821925ceff52dd
unknown <dedoa82.as@gmail.com>
2022-05-06 17:46:55 +0200

    Create : Seeder srtucture -> products, categories, suppliers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Supplier;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // factory(Category::class, 10)->create();
+        factory(Supplier::class, 10)->create();
+        factory(Product::class, 10)->create();
     }
 }
