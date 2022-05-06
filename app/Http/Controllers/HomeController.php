837a0ced2c8db73ac81fd2cc5fc83fdfc2555218
unknown <dedoa82.as@gmail.com>
2022-05-06 18:28:17 +0200

    Home view :: Fetch data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,8 +15,19 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $products = DB::table('product_handler')->get();
+
+        $men_products = [];
+        $women_products = [];
+
+        foreach ($products as $product) 
+            $product->gender 
+            ? array_push($men_products, $product) 
+            : array_push($women_products, $product) ;
+
         return view('index', [
-            ''
-        ])
+            'men_products' => $men_products,
+            'womens_products' => $women_products,
+        ]);
     }
 }
