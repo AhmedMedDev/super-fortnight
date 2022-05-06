691cd1cb346e743f71292f511158edbd40f481a8
unknown <dedoa82.as@gmail.com>
2022-05-06 19:24:22 +0200

    Shop view :: display data

--- app/Http/Livewire/Main/Shop.php
+++ app/Http/Livewire/Main/Shop.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Livewire\Main;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Shop extends Component
 {
+    public $products;
+
+    public function mount()
+    {
+        $this->products = Cache::rememberForever('products', 
+        fn () => DB::table('product_handler')->get());
+    }
+
     public function render()
     {
         return view('livewire.main.shop');
